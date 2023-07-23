f3ff60f221cc17f91eba16a0b15a5588181775b9
jswan <jswannieman@gmail.com>
2023-07-23 15:36:31 +1200

    Update App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
 import React, { useState, useEffect } from "react";
 
-function App() {
-  return <div className="App"></div>;
-}
+export default function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+  const [windowOuterWidth, setWindowOuterWidth] = useState(window.outerWidth);
+  const [windowOuterHeight, setWindowOuterHeight] = useState(
+    window.outerHeight
+  );
+  const [windowDocument, setWindowDocument] = useState(window.document.URL);
+  const [counter, setCounter] = useState(0);
 
-export default App;
+  const handleWindowChange = () => {
+    setWindowWidth(window.innerWidth);
+    setWindowHeight(window.innerHeight);
+    setWindowOuterWidth(window.outerWidth);
+    setWindowOuterHeight(window.outerHeight);
+    setWindowDocument(window.document.URL);
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowChange);
+    };
+  }, []);
+
+  return (
+    <div
+      className="App"
+      style={{
+        background: "lightyellow",
+        height: "95vh",
+        border: "10px solid #000",
+        borderStyle: "double",
+      }}
+    >
+      <p style={{ background: "skyblue" }}>Window Inner Width: {windowWidth}</p>
+      <p style={{ background: "crimson" }}>
+        Window Inner Height: {windowHeight}
+      </p>
+      <p style={{ background: "gray" }}>
+        Window Outer Width: {windowOuterWidth}
+      </p>
+      <p style={{ background: "peachpuff" }}>
+        Window Outer Height: {windowOuterHeight}
+      </p>
+      <p style={{ background: "lightgreen" }}>
+        Window Document: {windowDocument}
+      </p>
+      <p style={{ color: "#FFF", background: "black" }}>
+        Window Size has now changed {counter} times
+      </p>
+    </div>
+  );
+}
